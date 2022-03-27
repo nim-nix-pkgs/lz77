e8e5fdf41e78c37e796c5f0ee7c1e0078315b3a5
hugosenari <hugosenari@gmail.com>
2022-03-27 18:20:12 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-lz77-master.ref   = "refs/heads/master";
   inputs.src-lz77-master.owner = "sealmove";
   inputs.src-lz77-master.repo  = "LZ77";
+  inputs.src-lz77-master.dir   = "";
   inputs.src-lz77-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
